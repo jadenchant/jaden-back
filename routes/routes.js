bca4ad1e0336ef59a07378fbb7a19b8eda4f93f3
Jaden Chant <chant1@stolaf.edu>
2022-09-01 11:58:02 -0500

    removed get api

--- routes/routes.js
+++ routes/routes.js
@@ -77,13 +77,13 @@ router.get("/getDistancePrev", async (req, res) => {
 });
 
 //Get by ID Method
-router.get("/getOne/:id", async (req, res) => {
-  try {
-    const data = await Model.findById(req.params.id);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// router.get("/getOne/:id", async (req, res) => {
+//   try {
+//     const data = await Model.findById(req.params.id);
+//     res.json(data);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
